120a138cbea0fa359ded6413d1a0a88aafe4d651
Lautaro Bachmann <laurigbach@gmail.com>
2021-11-25 16:04:00 -0300

    Added lyricstranslate as a way to translate songs

--- backend/flask_api/musipy/lyrics.py
+++ backend/flask_api/musipy/lyrics.py
@@ -2,6 +2,7 @@ import lyricsgenius, re, pprint, csv, time
 from .config import genius_api_key
 from .translate import translate_verse
 from multiprocessing.dummy import Pool as ThreadPool
+from .lyricstranslate import lyricstranslate
 
 pool = ThreadPool(8)
 genius = lyricsgenius.Genius(genius_api_key)
@@ -12,28 +13,32 @@ def get_lyrics_list(song_title, author):
     except:
         return None
     regx = re.compile(r'\[.*\]|')
+    #TODO: Handle weird text on lyrics like URLCopyEmbedCopy
+    #      To reproduce the error use main function with "Auslander"
     cleaned_lyrics = re.sub(regx, '', lyrics.replace('\u2005', ' '))
     lyrics_list = cleaned_lyrics.split('\n')
     return [verse.strip() for verse in lyrics_list if verse]
 
-def get_lyrics_dict(song_title, author, trans_lang='en'):
+def get_lyrics_dict(song_title, author, trans_lang='english'):
     lang_dict = {
-        'es': ' (traducción al español)',
-        'en': ' (english translation)'
+        'español': ' (traducción al español)',
+        'english': ' (english translation)'
     }
     songs = [song_title, song_title + lang_dict[trans_lang]]
     lyrics = [get_lyrics_list(song, author) for song in songs]
+    is_invalid = lambda lyrics: not lyrics[1] or not len(lyrics[0]) == len(lyrics[1])
     if not lyrics[0]:
         print(f'No lyrics found for "{song_title}"\n')
         return None
-    if not lyrics[1] or not len(lyrics[0]) == len(lyrics[1]):
-        print('\nTranslating song...')
-        lyrics[1] = pool.map(translate_verse, lyrics[0])
+    if is_invalid(lyrics):
+        print('\nSearching song on lyricstranslate...')
+        lyrics[1] = lyricstranslate(author, song_title, trans_lang)
+        if is_invalid(lyrics):
+            print('\nTranslating song...')
+            lyrics[1] = pool.map(translate_verse, lyrics[0])
         
-        # res = {verse: translate_verse(verse, trans) for verse in lyrics}
-
     res = list(zip(lyrics[0], lyrics[1]))
     return res
 
 if __name__ == "__main__":
-    print(get_lyrics_trans("AUSLÄNDER", 'Rammstein', trans_lang='en'))
+    print(get_lyrics_dict("AUSLÄNDER", 'Rammstein', trans_lang='english'))
